a3ee5d7e9a1b871eac8b95a5701807d5fb6548d0
Sven (Sbte) <svenb.linux@gmail.com>
2011-06-07 21:37:05 +0200

    Add the total

--- statistics.py
+++ statistics.py
@@ -10,11 +10,17 @@ emeseneppa = launchpad.people['emesene-team'].getPPAByName(name='emesene-stable'
 print "Fetching data..."
 binaries = emeseneppa.getPublishedBinaries()
 printed_binaries = []
+total = 0
 for binary in binaries:
     if binary.binary_package_version not in printed_binaries:
+        count = binary.getDownloadCount()
         print '{0:{width}}{1:{width2}}{2}'.format(binary.binary_package_name,
             binary.binary_package_version,
             str(binary.getDownloadCount()),
             width=20, width2=30)
         printed_binaries.append(binary.binary_package_version)
+        if binary.binary_package_name == 'emesene':
+            total += count
+print 'Total amount of emesene downloads:'
+print total
 print "Done."
